7589985253b74ce4005a0c6fa397dc48e938c3c9
Santanu <santunuachary@gmail.com>
2019-08-20 11:21:38 +0530

    added Hello.java

--- src/com/automation/ChromeTest.java
+++ src/com/automation/ChromeTest.java
@@ -7,9 +7,7 @@ public class ChromeTest
 	public static void main(String[] args) throws Exception
 		{
 			System.setProperty("webdriver.chrome.driver","drivers\\chromedriver.exe");
-			ChromeDriver driver;
-			//ChromeDriver driver = null; //primitive type
-			driver= new ChromeDriver();
+			ChromeDriver driver= new ChromeDriver();
 			Thread.sleep(5000);
 			driver.close();
 			
